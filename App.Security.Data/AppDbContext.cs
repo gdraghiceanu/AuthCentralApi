ef67f050da6b043e0b16c771aa09372d0f407705
George Draghiceanu <georgedr@playtika.com>
2023-11-12 09:50:04 +0200

    Add connection string and seeding daata

--- App.Security.Data/AppDbContext.cs
+++ App.Security.Data/AppDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using App.Security.Helper;
 
 namespace App.Security.Data
 {
@@ -9,6 +10,50 @@ namespace App.Security.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AppUserClaim>().HasData(
+                new AppUserClaim() { Id = 1, ClaimType = Constants.ROLE_ADMIN, ClaimValue = Constants.ADD_USER_CLAIM_VALUE, DateCreated = DateTime.UtcNow, DateModified = DateTime.UtcNow },
+                new AppUserClaim() { Id = 2, ClaimType = Constants.ROLE_ADMIN, ClaimValue = Constants.EDIT_USER_CLAIM_VALUE, DateCreated = DateTime.UtcNow, DateModified = DateTime.UtcNow },
+                new AppUserClaim() { Id = 3, ClaimType = Constants.ROLE_ADMIN, ClaimValue = Constants.DELETE_USER_CLAIM_VALUE, DateCreated = DateTime.UtcNow, DateModified = DateTime.UtcNow },
+                new AppUserClaim() { Id = 4, ClaimType = Constants.ROLE_ADMIN, ClaimValue = Constants.GET_USER_CLAIM_VALUE, DateCreated = DateTime.UtcNow, DateModified = DateTime.UtcNow },
+
+                new AppUserClaim() { Id = 5, ClaimType = Constants.ROLE_USER, ClaimValue = Constants.ADD_USER_CLAIM_VALUE, DateCreated = DateTime.UtcNow, DateModified = DateTime.UtcNow },
+                new AppUserClaim() { Id = 6, ClaimType = Constants.ROLE_USER, ClaimValue = Constants.EDIT_USER_CLAIM_VALUE, DateCreated = DateTime.UtcNow, DateModified = DateTime.UtcNow },
+                new AppUserClaim() { Id = 7, ClaimType = Constants.ROLE_USER, ClaimValue = Constants.DELETE_USER_CLAIM_VALUE, DateCreated = DateTime.UtcNow, DateModified = DateTime.UtcNow },
+                new AppUserClaim() { Id = 8, ClaimType = Constants.ROLE_USER, ClaimValue = Constants.GET_USER_CLAIM_VALUE, DateCreated = DateTime.UtcNow, DateModified = DateTime.UtcNow }
+                );
+
+
+            builder.Entity<IdentityRole<int>>().HasData(
+                new IdentityRole<int>() { Id = 1, Name = Constants.ROLE_ADMIN, NormalizedName = Constants.ROLE_ADMIN.ToUpper() },
+                new IdentityRole<int>() { Id = 2, Name = Constants.ROLE_USER, NormalizedName = Constants.ROLE_USER.ToUpper() }
+                );
+
+            var adminUser = new AppUser() { Id = 1, UserName = "admin@admin.com", Email = "admin@admin.com", FullName = "Admin", DateCreated = DateTime.UtcNow, DateModified = DateTime.UtcNow };
+
+            adminUser.PasswordHash = new PasswordHasher<AppUser>().HashPassword(adminUser, "1234!@#$");
+            builder.Entity<AppUser>().HasData(adminUser);
+
+            builder.Entity<IdentityUserRole<int>>().HasData(
+                new IdentityUserRole<int>() { UserId = 1, RoleId = 1 }
+                );
+
+            builder.Entity<IdentityUserClaim<int>>().HasData(
+                new IdentityUserClaim<int>() { Id = 1, UserId = 1, ClaimType = Constants.ROLE_ADMIN, ClaimValue = Constants.ADD_USER_CLAIM_VALUE },
+                new IdentityUserClaim<int>() { Id = 2, UserId = 1, ClaimType = Constants.ROLE_ADMIN, ClaimValue = Constants.EDIT_USER_CLAIM_VALUE },
+                new IdentityUserClaim<int>() { Id = 3, UserId = 1, ClaimType = Constants.ROLE_ADMIN, ClaimValue = Constants.DELETE_USER_CLAIM_VALUE },
+                new IdentityUserClaim<int>() { Id = 4, UserId = 1, ClaimType = Constants.ROLE_ADMIN, ClaimValue = Constants.GET_USER_CLAIM_VALUE },
+
+                new IdentityUserClaim<int>() { Id = 5, UserId = 1, ClaimType = Constants.ROLE_USER, ClaimValue = Constants.ADD_USER_CLAIM_VALUE },
+                new IdentityUserClaim<int>() { Id = 6, UserId = 1, ClaimType = Constants.ROLE_USER, ClaimValue = Constants.EDIT_USER_CLAIM_VALUE },
+                new IdentityUserClaim<int>() { Id = 7, UserId = 1, ClaimType = Constants.ROLE_USER, ClaimValue = Constants.DELETE_USER_CLAIM_VALUE },
+                new IdentityUserClaim<int>() { Id = 8, UserId = 1, ClaimType = Constants.ROLE_USER, ClaimValue = Constants.GET_USER_CLAIM_VALUE }
+                );
+
+            base.OnModelCreating(builder);
         }
     }
 }
---
+++ App.Security.Helper/Constats.cs
@@ -0,0 +1,15 @@
+﻿namespace App.Security.Helper
+{
+    public class Constants
+    {
+        public static readonly string ROLE_ADMIN = "Admin";
+        public static readonly string ROLE_USER = "User";
+
+        public static readonly string ADD_USER_CLAIM_VALUE = "Add User";
+        public static readonly string EDIT_USER_CLAIM_VALUE = "Edit User";
+        public static readonly string DELETE_USER_CLAIM_VALUE = "Delete User";
+        public static readonly string GET_USER_CLAIM_VALUE = "Get User";
+
+
+    }
+}
